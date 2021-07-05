1ef0737a01577e038460dd7477d1182dbcc1878d
toasterbirb <anton.meuronen@protonmail.com>
2021-07-05 13:55:19 +0300

    Updated getrefreshrate()

--- src/renderwindow.cpp
+++ src/renderwindow.cpp
@@ -115,12 +115,13 @@ SDL_Texture* RenderWindow::renderTextEntity(Entity& textEntity)
 
 int RenderWindow::getRefreshRate()
 {
-	int displayIndex = SDL_GetWindowDisplayIndex( window );
+	//int displayIndex = SDL_GetWindowDisplayIndex( window );
 
-	SDL_DisplayMode mode;
-	SDL_GetDisplayMode(displayIndex, 0, &mode);
+	//SDL_DisplayMode mode;
+	//SDL_GetDisplayMode(displayIndex, 0, &mode);
 
-	return mode.refresh_rate;
+	//return mode.refresh_rate;
+	return refresh_rate;
 }
 
 void RenderWindow::cleanUp()
