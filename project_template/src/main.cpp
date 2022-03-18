27aa7d884922998ea56ea21d6c14e26390e548b3
Toasterbirb <git@toasterbirb.com>
2022-03-18 20:03:44 +0200

    Added a game project template

---
+++ project_template/src/main.cpp
@@ -0,0 +1,38 @@
+#include <birb2d/Renderwindow.hpp>
+#include <birb2d/Timestep.hpp>
+
+int main(int argc, char** argv)
+{
+	Birb::Window window("Birb2D project template", Birb::Vector2int(1280, 720), 75, false);
+	Birb::TimeStep timeStep;
+	timeStep.Init(&window);
+
+	bool ApplicationRunning = true;
+	while (ApplicationRunning)
+	{
+		timeStep.Start();
+		while (timeStep.Running())
+		{
+			/* Handle input stuff */
+			while (window.PollEvents())
+			{
+				window.EventTick(window.event, &ApplicationRunning);
+			}
+
+			timeStep.Step();
+		}
+
+		window.Clear();
+		/* Handle rendering */
+
+		
+
+		/* End of rendering */
+		window.Display();
+
+		timeStep.End();
+	}
+
+	window.Cleanup();
+	return 0;
+}
