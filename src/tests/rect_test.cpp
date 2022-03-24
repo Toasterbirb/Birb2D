10ed3259592c115601170129bbbcd708a19e7f6c
Toasterbirb <git@toasterbirb.com>
2022-03-24 15:43:21 +0200

    Wrote some more tests to clear up some confusion

--- src/tests/rect_test.cpp
+++ src/tests/rect_test.cpp
@@ -40,6 +40,7 @@ namespace Birb
 		CHECK(polygonA.points[2] == Vector2f(10, 10));
 		CHECK(polygonA.points[3] == Vector2f(0, 10));
 
+
 		Rect rectB(0, 0, -10, -10);
 		Polygon polygonB = rectB.toPolygon();
 
@@ -47,6 +48,24 @@ namespace Birb
 		CHECK(polygonB.points[1] == Vector2f(-10, 0));
 		CHECK(polygonB.points[2] == Vector2f(-10, -10));
 		CHECK(polygonB.points[3] == Vector2f(0, -10));
+
+
+		Rect rectC(2, 2, 1, 3);
+		Polygon polygonC = rectC.toPolygon();
+
+		CHECK(polygonC.points[0] == Vector2f(2, 2));
+		CHECK(polygonC.points[1] == Vector2f(3, 2));
+		CHECK(polygonC.points[2] == Vector2f(3, 5));
+		CHECK(polygonC.points[3] == Vector2f(2, 5));
+
+
+		Rect rectD(6, 5, 2, 1);
+		Polygon polygonD = rectD.toPolygon();
+
+		CHECK(polygonD.points[0] == Vector2f(6, 5));
+		CHECK(polygonD.points[1] == Vector2f(8, 5));
+		CHECK(polygonD.points[2] == Vector2f(8, 6));
+		CHECK(polygonD.points[3] == Vector2f(6, 6));
 	}
 
 	TEST_CASE("Rect comparison")
