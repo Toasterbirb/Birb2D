63a70e1e797073b86604ef81e5821b8f518ab442
Toasterbirb <git@toasterbirb.com>
2022-03-28 16:24:45 +0300

    Continuous valgrind testing

--- continuous_testing.sh
+++ continuous_testing.sh
@@ -4,13 +4,24 @@ function quick()
 	find ./src ./include | entr -sc 'make -j$(nproc) run_quick_tests'
 }
 
+function quick_valgrind()
+{
+	find ./src ./include | entr -sc 'make -j$(nproc) run_quick_tests_valgrind'
+}
+
 function full()
 {
 	find ./src ./include | entr -sc 'make -j$(nproc) run_tests'
 }
 
+function full_valgrind()
+{
+	find ./src ./include | entr -sc 'make -j$(nproc) run_tests_valgrind'
+}
+
 case $1 in
 	quick) quick ;;
-	full) full ;;
+	quick_valgrind) quick_valgrind ;;
+	full_valgrind) full_valgrind ;;
 	*) echo "Run this script with either argument 'quick' or 'full'" ;;
 esac
