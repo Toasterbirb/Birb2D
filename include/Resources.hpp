7bf54a18a791bb50f46001afc7d43dad29bd3ee9
Toasterbirb <git@toasterbirb.com>
2022-04-03 12:23:29 +0300

    LoadFont isn't needed anymore in here

--- include/Resources.hpp
+++ include/Resources.hpp
@@ -14,7 +14,6 @@ namespace Birb
 		static SDL_Texture* LoadTexture(const std::string& p_filePath);
 		static SDL_Texture* TextSprite(const std::string& text, TTF_Font* font, const SDL_Color& color);
 		static SDL_Texture* TextSprite(const std::string& text, TTF_Font* font, const SDL_Color& color, const SDL_Color& bgColor);
-		static TTF_Font* 	LoadFont(const std::string& p_filePath, const int& p_fontSize);
 		static Uint8* 		CopyTexturePixels(SDL_Surface* surface, int* width, int* height, int* pitch);
 	};
 }
