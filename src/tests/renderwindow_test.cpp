4b640196e04d4f5a2d1bdcbe16f428bc7bfe63d8
Toasterbirb <git@toasterbirb.com>
2022-04-04 14:36:25 +0300

    Starting a test rework

---
+++ src/tests/renderwindow_test.cpp
@@ -0,0 +1,114 @@
+#include <memory>
+#include "doctest.h"
+#include "Renderwindow.hpp"
+#include "Values.hpp"
+#include "Scene.hpp"
+#include "Timestep.hpp"
+
+namespace Birb
+{
+	TEST_CASE("Rendering: Renderwindow")
+	{
+		Window window("Birb2D tests", Vector2int(1280, 720), 75, false);
+
+		CHECK(window.refresh_rate == 75);
+		CHECK(window.dimensions == Vector2int(1280, 720));
+		CHECK(window.win_title == "Birb2D tests");
+		CHECK(window.original_window_dimensions == Vector2int(1280, 720));
+
+		CHECK(Global::RenderVars::Renderer == window.renderer);
+		CHECK(Global::RenderVars::RefreshRate == 75);
+		CHECK(window.win == Global::RenderVars::MainWindow);
+		CHECK(Global::RenderVars::BackgroundColor == Colors::Black);
+	}
+
+	TEST_CASE("Rendering: General")
+	{
+		Window window("Birb2D tests", Vector2int(1280, 720), 75, false);
+		ApplicationInfo appInfo("Birb2D_tests");
+		Scene scene;
+		scene.Activate();
+
+		/* Place to store stuff in subcases */
+		std::vector<Entity> entities;
+		std::vector<Circle> circles;
+
+		SUBCASE("Sprites / Textures in a scene")
+		{
+			const int ENTITY_COUNT = 5;
+			SDL_Texture* birbSprite = Resources::LoadTexture(appInfo.ResLocation + "/textures/birb.png");
+
+			/* Create the entities */
+			for (int i = 0; i < ENTITY_COUNT; i++)
+			{
+				Entity testEntity("Test entity", Vector2int(i * 70, 10), birbSprite);
+				entities.push_back(testEntity);
+			}
+
+			/* Add the entities to the scene */
+			for (int i = 0; i < entities.size(); i++)
+				scene.AddObject(&entities[i]);
+
+			CHECK(scene.ObjectCount() == ENTITY_COUNT);
+		}
+
+		SUBCASE("Scenes inside of other scenes")
+		{
+			/* Create a child scene */
+			Scene* childScene = new Scene();
+
+			/* Create two rects for the background */
+			Rect* rectA = new Rect(Rect(0, 0, 640, 720, Colors::LightGray));
+			Rect* rectB = new Rect(Rect(640, 0, 640, 720, Colors::DarkGray));
+			Rect* rectC = new Rect(Rect(320, 180, 640, 360, Colors::Red));
+
+			scene.AddObject(rectA);
+			scene.AddObject(rectB);
+
+			/* Create a rect to put into the child scene */
+			rectC->renderingPriority = 1;
+			childScene->AddObject(rectC);
+
+			/* Add the childscene as a SceneObject into the main scene */
+			childScene->renderingPriority = 1;
+			scene.AddObject(childScene);
+
+			CHECK(scene.ObjectCount() == 3);
+			CHECK(childScene->ObjectCount() == 1);
+		}
+
+		TimeStep timeStep;
+		timeStep.Init(&window);
+		Timer testTimer;
+		testTimer.Start();
+
+		CHECK(scene.isActive());
+
+		bool ApplicationRunning = true;
+		while (ApplicationRunning)
+		{
+			if (testTimer.ElapsedSeconds() > 0.5)
+				ApplicationRunning = false;
+
+			timeStep.Start();
+			while (timeStep.Running())
+			{
+				while (window.PollEvents())
+				{
+					window.EventTick(window.event, &ApplicationRunning);
+				}
+
+				timeStep.Step();
+			}
+
+			window.Clear();
+			/** Handle rendering **/
+
+			scene.Render();
+
+			/** End of rendering **/
+			window.Display();
+			timeStep.End();
+		}
+	}
+}
