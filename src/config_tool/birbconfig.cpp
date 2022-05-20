2367833617dff1e727b2e44b18fa57d621bf7fa6
Toasterbirb <git@toasterbirb.com>
2022-05-20 19:58:09 +0300

    Add explanations for the flags

--- src/config_tool/birbconfig.cpp
+++ src/config_tool/birbconfig.cpp
@@ -10,15 +10,15 @@ int main(void)
 	std::cout << "-- Birb2D config tool --\n\nBirb2D has been compiled with the following flags:" << std::endl;
 
 #ifdef DEBUG
-	std::cout << "- DEBUG" << std::endl;
+	std::cout << "- DEBUG\t\tMore verbosity" << std::endl;
 #endif
 
 #ifdef LIB_SDL
-	std::cout << "- LIB_SDL" << std::endl;
+	std::cout << "- LIB_SDL\tUse SDL2 as the graphics + sound library" << std::endl;
 #endif
 
 #ifdef NO_SOUND
-	std::cout << "- NO_SOUND" << std::endl;
+	std::cout << "- NO_SOUND\tDisables sound completely (gets rid of SDL2_mixer dependency)" << std::endl;
 #endif
 
 	return 0;
