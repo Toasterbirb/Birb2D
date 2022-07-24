c2c63b030d21150ab7f756785d759c733dece573
Toasterbirb <git@toasterbirb.com>
2022-07-25 00:55:39 +0300

    Project tree restructuring

---
+++ examples/hello_world/src/main.cpp
@@ -0,0 +1,39 @@
+#include "Renderwindow.hpp"
+#include "Timestep.hpp"
+
+int main(int argc, char** argv)
+{
+	Birb::Debug::Log("Creating the window");
+	Birb::Window window("Birb2D project template", Birb::Vector2int(1280, 720), 75, false);
+	Birb::TimeStep timeStep;
+	timeStep.Init(&window);
+
+	Birb::Debug::Log("Starting the game loop");
+	bool ApplicationRunning = true;
+	while (ApplicationRunning)
+	{
+		timeStep.Start();
+		while (timeStep.Running())
+		{
+			/* Handle input stuff */
+			while (window.PollEvents())
+			{
+				window.EventTick(window.event, &ApplicationRunning);
+			}
+
+			timeStep.Step();
+		}
+
+		window.Clear();
+		/* Handle rendering */
+
+
+
+		/* End of rendering */
+		window.Display();
+
+		timeStep.End();
+	}
+
+	return 0;
+}
