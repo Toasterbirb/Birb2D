8a5028e3bda6b0d09f585f92ec566ec70797f459
Toasterbirb <git@toasterbirb.com>
2022-08-17 15:22:50 +0300

    Create the base for level class tests

---
+++ tests/level_test.cpp
@@ -0,0 +1,19 @@
+#include "doctest.h"
+#include "Level.hpp"
+
+namespace Birb
+{
+	TEST_CASE("Setting tiles")
+	{
+		Vector2int level_dimensions = { 10, 10 };
+		Level level(level_dimensions);
+
+		Level::Tile tileA;
+		tileA.is_collider = true;
+		level.SetTile({ 1, 1 }, tileA);
+
+		CHECK(level.GetTile({ 1, 1 }).is_collider);
+		CHECK_FALSE(level.GetTile({ 1, 1 }).is_empty);
+		CHECK(level.GetTile({ 1, 1 }).rect.color == 0x000050);
+	}
+}
