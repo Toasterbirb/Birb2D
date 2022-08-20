bec446e42b9a72d0d6947b2143efec1f5983e61c
Toasterbirb <git@toasterbirb.com>
2022-08-20 05:00:21 +0300

    Add a keycode enum to make life easier with SDL_keycodes
    Also included is a function that can convert SDL events into said
    keycodes

---
+++ include/Input.hpp
@@ -0,0 +1,66 @@
+#pragma once
+#include "SDL.h"
+
+namespace Birb
+{
+	namespace Input
+	{
+		enum KeyCode
+		{
+			NUM_0,
+			NUM_1,
+			NUM_2,
+			NUM_3,
+			NUM_4,
+			NUM_5,
+			NUM_6,
+			NUM_7,
+			NUM_8,
+			NUM_9,
+			A, B, C, D, E, F, G, H, I, J,
+			K, L, M, N, O, P, Q, R, S, T,
+			U, V, W, X, Y, Z,
+			BACKSPACE,
+			CAPS_LOCK,
+			CUT,
+			PASTE,
+			DELETE,
+			ARROW_DOWN, ARROW_UP, ARROW_LEFT, ARROW_RIGHT,
+			END,
+			EQUALS,
+			ESCAPE,
+			F1, F2, F3, F4, F5, F6, F7, F8, F9, F10, F11, F12,
+			F13, F14, F15, F16, F17, F18, F19, F20, F21, F22, F23, F24,
+			HOME,
+			KP_1, KP_2, KP_3, KP_4, KP_5,
+			KP_6, KP_7, KP_8, KP_9, KP_0,
+			ALT_LEFT, ALT_RIGHT,
+			CTRL_LEFT, CTRL_RIGHT,
+			SHIFT_LEFT, SHIFT_RIGHT,
+			PAGE_DOWN, PAGE_UP,
+			PRINT_SCREEN,
+			RETURN,
+			SEMICOLON,
+			SLASH,
+			SPACE,
+			TAB,
+			AMPERSAND,
+			ASTERISK,
+			AT,
+			CARET,
+			DOLLAR,
+			EXCLAIM,
+			GREATER_THAN, LESS_THAN,
+			HASH,
+			LEFT_PAREN, RIGHT_PAREN,
+			PERCENT,
+			PLUS,
+			QUESTION,
+			QUOTE,
+			UNDERSCORE,
+			NOT_RECOGNIZED
+		};
+
+		KeyCode EventToKeycode(const SDL_Event& event);
+	}
+}
---
+++ src/input.cpp
@@ -0,0 +1,370 @@
+#include "Input.hpp"
+
+namespace Birb
+{
+	namespace Input
+	{
+		KeyCode EventToKeycode(const SDL_Event& event)
+		{
+			/* Skip events that don't have anything to do with
+			 * keyboard input */
+			if (event.type != SDL_KEYDOWN && event.type != SDL_KEYUP)
+				return NOT_RECOGNIZED;
+
+			KeyCode code;
+
+			switch (event.key.keysym.scancode)
+			{
+				case (SDL_SCANCODE_0):
+					return NUM_0;
+
+				case (SDL_SCANCODE_1):
+					return NUM_1;
+
+				case (SDL_SCANCODE_2):
+					return NUM_2;
+
+				case (SDL_SCANCODE_3):
+					return NUM_3;
+
+				case (SDL_SCANCODE_4):
+					return NUM_4;
+
+				case (SDL_SCANCODE_5):
+					return NUM_5;
+
+				case (SDL_SCANCODE_6):
+					return NUM_6;
+
+				case (SDL_SCANCODE_7):
+					return NUM_7;
+
+				case (SDL_SCANCODE_8):
+					return NUM_8;
+
+				case (SDL_SCANCODE_9):
+					return NUM_9;
+
+				case (SDL_SCANCODE_A):
+					return A;
+
+				case (SDL_SCANCODE_B):
+					return B;
+
+				case (SDL_SCANCODE_C):
+					return C;
+
+				case (SDL_SCANCODE_D):
+					return D;
+
+				case (SDL_SCANCODE_E):
+					return E;
+
+				case (SDL_SCANCODE_F):
+					return F;
+
+				case (SDL_SCANCODE_G):
+					return G;
+
+				case (SDL_SCANCODE_H):
+					return H;
+
+				case (SDL_SCANCODE_I):
+					return I;
+
+				case (SDL_SCANCODE_J):
+					return J;
+
+				case (SDL_SCANCODE_K):
+					return K;
+
+				case (SDL_SCANCODE_L):
+					return L;
+
+				case (SDL_SCANCODE_M):
+					return M;
+
+				case (SDL_SCANCODE_N):
+					return N;
+
+				case (SDL_SCANCODE_O):
+					return O;
+
+				case (SDL_SCANCODE_P):
+					return P;
+
+				case (SDL_SCANCODE_Q):
+					return Q;
+
+				case (SDL_SCANCODE_R):
+					return R;
+
+				case (SDL_SCANCODE_S):
+					return S;
+
+				case (SDL_SCANCODE_T):
+					return T;
+
+				case (SDL_SCANCODE_U):
+					return U;
+
+				case (SDL_SCANCODE_V):
+					return V;
+
+				case (SDL_SCANCODE_W):
+					return W;
+
+				case (SDL_SCANCODE_X):
+					return X;
+
+				case (SDL_SCANCODE_Y):
+					return Y;
+
+				case (SDL_SCANCODE_Z):
+					return Z;
+
+				case (SDL_SCANCODE_BACKSPACE):
+					return BACKSPACE;
+
+				case (SDL_SCANCODE_CAPSLOCK):
+					return CAPS_LOCK;
+
+				case (SDL_SCANCODE_CUT):
+					return CUT;
+
+				case (SDL_SCANCODE_PASTE):
+					return PASTE;
+
+				case (SDL_SCANCODE_DELETE):
+					return DELETE;
+
+				case (SDL_SCANCODE_UP):
+					return ARROW_UP;
+
+				case (SDL_SCANCODE_DOWN):
+					return ARROW_DOWN;
+
+				case (SDL_SCANCODE_LEFT):
+					return ARROW_LEFT;
+
+				case (SDL_SCANCODE_RIGHT):
+					return ARROW_RIGHT;
+
+				case (SDL_SCANCODE_END):
+					return END;
+
+				case (SDL_SCANCODE_EQUALS):
+					return EQUALS;
+
+				case (SDL_SCANCODE_ESCAPE):
+					return ESCAPE;
+
+				case (SDL_SCANCODE_F1):
+					return F1;
+
+				case (SDL_SCANCODE_F2):
+					return F2;
+
+				case (SDL_SCANCODE_F3):
+					return F3;
+
+				case (SDL_SCANCODE_F4):
+					return F4;
+
+				case (SDL_SCANCODE_F5):
+					return F5;
+
+				case (SDL_SCANCODE_F6):
+					return F6;
+
+				case (SDL_SCANCODE_F7):
+					return F7;
+
+				case (SDL_SCANCODE_F8):
+					return F8;
+
+				case (SDL_SCANCODE_F9):
+					return F9;
+
+				case (SDL_SCANCODE_F10):
+					return F10;
+
+				case (SDL_SCANCODE_F11):
+					return F11;
+
+				case (SDL_SCANCODE_F12):
+					return F12;
+
+				case (SDL_SCANCODE_F13):
+					return F13;
+
+				case (SDL_SCANCODE_F14):
+					return F14;
+
+				case (SDL_SCANCODE_F15):
+					return F15;
+
+				case (SDL_SCANCODE_F16):
+					return F16;
+
+				case (SDL_SCANCODE_F17):
+					return F17;
+
+				case (SDL_SCANCODE_F18):
+					return F18;
+
+				case (SDL_SCANCODE_F19):
+					return F19;
+
+				case (SDL_SCANCODE_F20):
+					return F20;
+
+				case (SDL_SCANCODE_F21):
+					return F21;
+
+				case (SDL_SCANCODE_F22):
+					return F22;
+
+				case (SDL_SCANCODE_F23):
+					return F23;
+
+				case (SDL_SCANCODE_F24):
+					return F24;
+
+				case (SDL_SCANCODE_HOME):
+					return HOME;
+
+				case (SDL_SCANCODE_KP_1):
+					return KP_1;
+
+				case (SDL_SCANCODE_KP_2):
+					return KP_2;
+
+				case (SDL_SCANCODE_KP_3):
+					return KP_3;
+
+				case (SDL_SCANCODE_KP_4):
+					return KP_4;
+
+				case (SDL_SCANCODE_KP_5):
+					return KP_5;
+
+				case (SDL_SCANCODE_KP_6):
+					return KP_6;
+
+				case (SDL_SCANCODE_KP_7):
+					return KP_7;
+
+				case (SDL_SCANCODE_KP_8):
+					return KP_8;
+
+				case (SDL_SCANCODE_KP_9):
+					return KP_9;
+
+				case (SDL_SCANCODE_KP_0):
+					return KP_0;
+
+				case (SDL_SCANCODE_LALT):
+					return ALT_LEFT;
+
+				case (SDL_SCANCODE_RALT):
+					return ALT_RIGHT;
+
+				case (SDL_SCANCODE_LCTRL):
+					return CTRL_LEFT;
+
+				case (SDL_SCANCODE_RCTRL):
+					return CTRL_RIGHT;
+
+				case (SDL_SCANCODE_LSHIFT):
+					return SHIFT_LEFT;
+
+				case (SDL_SCANCODE_RSHIFT):
+					return SHIFT_RIGHT;
+
+				case (SDL_SCANCODE_PAGEDOWN):
+					return PAGE_DOWN;
+
+				case (SDL_SCANCODE_PAGEUP):
+					return PAGE_UP;
+
+				case (SDL_SCANCODE_PRINTSCREEN):
+					return PRINT_SCREEN;
+
+				case (SDL_SCANCODE_RETURN):
+					return RETURN;
+
+				case (SDL_SCANCODE_SEMICOLON):
+					return SEMICOLON;
+
+				case (SDL_SCANCODE_SLASH):
+					return SLASH;
+
+				case (SDL_SCANCODE_SPACE):
+					return SPACE;
+
+				case (SDL_SCANCODE_TAB):
+					return TAB;
+
+				default:
+					break;
+			}
+
+			switch (event.key.keysym.mod)
+			{
+				case (SDLK_AMPERSAND):
+					return AMPERSAND;
+
+				case (SDLK_ASTERISK):
+					return ASTERISK;
+
+				case (SDLK_AT):
+					return AT;
+
+				case (SDLK_CARET):
+					return CARET;
+
+				case (SDLK_DOLLAR):
+					return DOLLAR;
+
+				case (SDLK_EXCLAIM):
+					return EXCLAIM;
+
+				case (SDLK_GREATER):
+					return GREATER_THAN;
+
+				case (SDLK_LESS):
+					return LESS_THAN;
+
+				case (SDLK_HASH):
+					return HASH;
+
+				case (SDLK_LEFTPAREN):
+					return LEFT_PAREN;
+
+				case (SDLK_RIGHTPAREN):
+					return RIGHT_PAREN;
+
+				case (SDLK_PERCENT):
+					return PERCENT;
+
+				case (SDLK_PLUS):
+					return PLUS;
+
+				case (SDLK_QUESTION):
+					return QUESTION;
+
+				case (SDLK_QUOTE):
+					return QUOTE;
+
+				case (SDLK_UNDERSCORE):
+					return UNDERSCORE;
+
+				default:
+					return NOT_RECOGNIZED;
+			}
+
+			return code;
+		}
+	}
+}
