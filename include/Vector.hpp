1c3baa0fe39de18edf7b38b1f614027ab237c8d5
realQuartzi <34374881+realQuartzi@users.noreply.github.com>
2022-10-05 20:14:42 +0200

    Updated Vector Include

---
+++ include/Vector.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "Vector/Vector3.hpp"
+#include "Vector/Vector3Int.hpp"
+#include "Vector/Vector2.hpp"
+#include "Vector/Vector2Int.hpp"
--- include/Vector/Vector.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "Vector3.hpp"
-#include "Vector3Int.hpp"
-#include "Vector2.hpp"
-#include "Vector2Int.hpp"
