9b5e1a5b8b28cb0f4b5ac9d705ad934fc9ee9046
Toasterbirb <git.53jvg@aleeas.com>
2022-10-31 15:57:47 +0200

    More asset manager testing

--- tests/assetmanager_test.cpp
+++ tests/assetmanager_test.cpp
@@ -10,8 +10,16 @@ namespace Birb
 		Texture birb_texture("./res/textures/birb.png");
 
 		AssetManager asset_manager;
+
+		/* Load a texture with the asset manager */
 		asset_manager.LoadTexture("birb", "./res/textures/birb.png");
 		CHECK(asset_manager.texture("birb").isLoaded());
 		CHECK(asset_manager.texture("birb").dimensions() == Vector2Int(64, 64));
+
+		/* Copy an existing texture into the asset manager */
+		asset_manager.AddTexture("existing_birb_texture", birb_texture);
+		CHECK(asset_manager.texture("existing_birb_texture").isLoaded());
+		CHECK(asset_manager.texture("existing_birb_texture").dimensions() == Vector2Int(64, 64));
+		CHECK(asset_manager.texture("existing_birb_texture") == birb_texture);
 	}
 }
