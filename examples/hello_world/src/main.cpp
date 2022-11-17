8710c3e233007d7e4fa1f0a38a1faf66d1e7245e
Toasterbirb <git@toasterbirb.com>
2022-11-17 02:16:38 +0200

    Get rid of unused parameters

--- examples/hello_world/src/main.cpp
+++ examples/hello_world/src/main.cpp
@@ -1,6 +1,6 @@
 #include "Birb2D.hpp"
 
-int main(int argc, char** argv)
+int main(void)
 {
 	Birb::Debug::Log("Creating the window");
 	Birb::Window window("Birb2D project template", Birb::Vector2Int(1280, 720), 75, false);
