06564a6c82d228989472b10ba2488265f50047c0
Toasterbirb <git@toasterbirb.com>
2022-12-15 17:43:13 +0200

    Add comments for sceneobject variables

--- include/SceneObject.hpp
+++ include/SceneObject.hpp
@@ -10,10 +10,20 @@ namespace Birb
 		SceneObject();
 		SceneObject(const int& priority);
 		SceneObject(const int& priority, const bool& active);
+
+		/// The rendering priority determnies the order that objects are rendered with
 		int renderingPriority;
+
+		/// Tells if the parent scene is active or not
 		bool sceneActive;
+
+		/// Is the object "active". Could mean visibility etc.
 		bool active;
+
+		/// Object is positioned in world space
 		bool world_space;
+
+		/// The position of the object is multiplied to achieve a parallax effect
 		float parallax_multiplier;
 
 		virtual void RenderFunc() = 0;
