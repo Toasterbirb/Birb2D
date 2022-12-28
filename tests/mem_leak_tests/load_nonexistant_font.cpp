b181aae826fb66fd27ed0571fefa2b7d1d530ce0
Toasterbirb <git.53jvg@aleeas.com>
2022-12-28 16:20:34 +0200

    Clear the SDL error buffer

--- tests/mem_leak_tests/load_nonexistant_font.cpp
+++ tests/mem_leak_tests/load_nonexistant_font.cpp
@@ -5,5 +5,9 @@ int main(void)
 {
 	std::cout << " > Load a font that doesn't exist and quit the program immediately" << std::endl;
 	Birb::Font font("some_font_that_doesn't_exist.ttf", 32);
+
+	/* Clear the SDL error buffer since it can be a bit leaky */
+	SDL_TLSCleanup();
+
 	return 0;
 }
