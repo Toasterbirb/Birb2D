b9fef21e9a5e830daec4111ecd7530b498d8ba28
Toasterbirb <git.53jvg@aleeas.com>
2023-01-02 11:43:39 +0200

    Some namespace declaration cleanup

--- tests/general_tests/logger_test.cpp
+++ tests/general_tests/logger_test.cpp
@@ -4,16 +4,13 @@
 
 namespace Birb
 {
-	namespace Debug
+	TEST_CASE("Logging")
 	{
-		TEST_CASE("Logging")
-		{
-			std::cout << "There should be 4 different log messages showing up shortly..." << std::endl;
-			CHECK_NOTHROW(Debug::Log("Log (this is only a test)"));
-			CHECK_NOTHROW(Debug::Log("Log (this is only a test)", Birb::Debug::log));
-			CHECK_NOTHROW(Debug::Log("Warning (this is only a test)", Birb::Debug::warning));
-			CHECK_NOTHROW(Debug::Log("Error (this is only a test)", Birb::Debug::error));
-			CHECK_NOTHROW(Debug::Log("Fixme (this is only a test)", Birb::Debug::fixme));
-		}
+		std::cout << "There should be 4 different log messages showing up shortly..." << std::endl;
+		CHECK_NOTHROW(Debug::Log("Log (this is only a test)"));
+		CHECK_NOTHROW(Debug::Log("Log (this is only a test)", Birb::Debug::log));
+		CHECK_NOTHROW(Debug::Log("Warning (this is only a test)", Birb::Debug::warning));
+		CHECK_NOTHROW(Debug::Log("Error (this is only a test)", Birb::Debug::error));
+		CHECK_NOTHROW(Debug::Log("Fixme (this is only a test)", Birb::Debug::fixme));
 	}
 }
