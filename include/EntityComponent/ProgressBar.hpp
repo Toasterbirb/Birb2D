5b6dae6ad89790dbcfc4483aa261e9a7bb23eeec
Toasterbirb <git.53jvg@aleeas.com>
2023-01-05 00:18:21 +0200

    Progress bars need some way to access their parent entities

--- include/EntityComponent/ProgressBar.hpp
+++ include/EntityComponent/ProgressBar.hpp
@@ -4,12 +4,16 @@
 
 namespace Birb
 {
+	class Entity;
+	struct Vector2;
+
 	namespace EntityComponent
 	{
 		/// ProgressBar is used to create... well... progress bars
 		/// If the entity has a sprite on it. That will be used instead of the fill color
-		struct ProgressBar
+		class ProgressBar
 		{
+		public:
 			ProgressBar();
 			ProgressBar(const int& p_borderWidth, Color* p_borderColor, Color* p_backgroundColor, Color* p_fillColor);
 			ProgressBar(const int& p_borderWidth, Color* p_borderColor, Color* p_backgroundColor, Color* p_fillColor, const int& p_minValue, const int& p_maxValue, const int& p_value);
@@ -24,6 +28,12 @@ namespace Birb
 			float maxValue; ///< Value where the progress bar should be full
 			float value; ///< Current progress value
 
+			/// Set the value so that the fill bar would reach the
+			/// given point. Nothing will happen if the point given
+			/// is outside of the Entity surface area
+			void SetValueFromRelativePoint(Vector2 point);
+			Entity* parent_entity;
+
 			bool operator==(const ProgressBar& other) const
 			{
 				return 	(borderWidth 		== other.borderWidth
@@ -33,6 +43,8 @@ namespace Birb
 						&& minValue 		== other.minValue
 						&& maxValue 		== other.maxValue);
 			}
+
+		private:
 		};
 	}
 }
--- src/EntityComponent/progressbar.cpp
+++ src/EntityComponent/progressbar.cpp
@@ -1,4 +1,8 @@
+#include "Entity.hpp"
 #include "EntityComponent/ProgressBar.hpp"
+#include "Logger.hpp"
+#include "Physics.hpp"
+#include "Vector/Vector2.hpp"
 
 namespace Birb
 {
@@ -16,6 +20,7 @@ namespace Birb
 			value = 0;
 
 			active = false;
+			parent_entity = nullptr;
 		}
 
 		ProgressBar::ProgressBar(const int& p_borderWidth, Color* p_borderColor, Color* p_backgroundColor, Color* p_fillColor)
@@ -26,12 +31,31 @@ namespace Birb
 			value = 0.25f;
 
 			active = true;
+			parent_entity = nullptr;
 		}
 
 		ProgressBar::ProgressBar(const int& p_borderWidth, Color* p_borderColor, Color* p_backgroundColor, Color* p_fillColor, const int& p_minValue, const int& p_maxValue, const int& p_value)
 		:borderWidth(p_borderWidth), borderColor(p_borderColor), backgroundColor(p_backgroundColor), fillColor(p_fillColor), minValue(p_minValue), maxValue(p_maxValue), value(p_value)
 		{
 			active = true;
+			parent_entity = nullptr;
+		}
+
+		void ProgressBar::SetValueFromRelativePoint(Vector2 point)
+		{
+			/* Check if the parent entity has been set */
+			// FIXME: Add a more elegant way to make th progressbars aware of their parent entity
+			if (parent_entity == nullptr)
+			{
+				Debug::Log("Set the slider parent entity first before using SetValueFromRelativePoint()", Debug::fixme);
+				return;
+			}
+
+			/* First test if the point is even on the Entity */
+			if (!Physics::PointInRect(parent_entity->rect, point))
+				return;
+
+			value = ((point.x - parent_entity->rect.x) / parent_entity->rect.w) * maxValue;
 		}
 	}
 }
