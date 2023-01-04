95e3892edaca9d603afe2a1d9ed3671766b64484
Toasterbirb <git.53jvg@aleeas.com>
2023-01-04 06:26:09 +0200

    Finish this test some day

--- tests/general_tests/graph_test.cpp
+++ tests/general_tests/graph_test.cpp
@@ -1,7 +1,10 @@
 #include "doctest.h"
 #include "Graphs.hpp"
 
-TEST_CASE("Graph comparison")
+namespace BirbTest
 {
-	// TODO: Implement this test :bird:
+	TEST_CASE("Graph comparison")
+	{
+		// TODO: Implement this test :bird:
+	}
 }
