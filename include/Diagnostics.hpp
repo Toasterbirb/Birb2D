b16dcda0b9fc61c253e808bbb8afc3fd74380287
Toasterbirb <git@toasterbirb.com>
2023-01-09 23:38:18 +0200

    Move the frametime graph into its own files entirely

--- include/Diagnostics.hpp
+++ include/Diagnostics.hpp
@@ -4,32 +4,13 @@
 #include "Graphs.hpp"
 #include "Timestep.hpp"
 
+/* Diagnostics related headers */
+#include "FrametimeGraph.hpp"
+
 namespace Birb
 {
 	namespace Diagnostics
 	{
-		/* --------------------------------- */
-		/* Implemented in frametimegraph.cpp */
-		/* --------------------------------- */
-		class FrametimeGraph
-		{
-		public:
-			FrametimeGraph(const Rect& rect, const int& pointCount, TimeStep& timeStep);
-			void Render();
-			bool enabled;
-			int pointCount;
-			Rect rect;
-
-			Font font;
-			Entity fps_text;
-			Entity frametime_text;
-			Entity framebudget_text;
-
-		private:
-			TimeStep& timeStep;
-			Widgets::Graph graph;
-		};
-
 		/// Boolean toggles for debugging features. Some can clutter the console output
 		/// quite a bit, so be careful
         struct Debugging
--- src/frametimegraph.cpp
+++ src/frametimegraph.cpp
@@ -1,4 +1,4 @@
-#include "Diagnostics.hpp"
+#include "FrametimeGraph.hpp"
 
 namespace Birb
 {
