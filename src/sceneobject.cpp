342a7f65b8413ba8b9268387de4f5885bae23372
Toasterbirb <git@toasterbirb.com>
2023-01-13 04:33:22 +0200

    SceneObject constructor cleanup

--- src/sceneobject.cpp
+++ src/sceneobject.cpp
@@ -3,27 +3,14 @@
 namespace Birb
 {
 	SceneObject::SceneObject()
-	{
-		renderingPriority = 0;
-		sceneActive = true;
-		active 		= true;
-		world_space = true;
-		parallax_multiplier = 1;
-	}
+	:renderingPriority(0), sceneActive(true), active(true), world_space(true), parallax_multiplier(1)
+	{}
 
 	SceneObject::SceneObject(const int& priority)
-	:renderingPriority(priority)
-	{
-		sceneActive = true;
-		active 		= true;
-		world_space = true;
-		parallax_multiplier = 1;
-	}
+	:renderingPriority(priority), sceneActive(true), active(true), world_space(true), parallax_multiplier(1)
+	{}
 
 	SceneObject::SceneObject(const int& priority, const bool& active)
-	:renderingPriority(priority), active(active)
-	{
-		world_space = true;
-		parallax_multiplier = 1;
-	}
+	:renderingPriority(priority), active(active), world_space(true), parallax_multiplier(1)
+	{}
 }
